6b32b59e8a8664231d5b41d177c7072e40286588
Revaldyputra <115059417+RevaldyPutra@users.noreply.github.com>
2022-10-24 20:54:34 +0700

    Update index.php

--- tugas-oop/index.php
+++ tugas-oop/index.php
@@ -1,7 +1,7 @@
 <?php
 echo "<center><h2>Tugas oop Revaldy Putra Pradipta Barus</h2></center>";
 echo "<br>";
-echo "<hr>;
+echo "<hr>";
 require ('animal.php');
 echo "<br>";
 require ('frog.php');
